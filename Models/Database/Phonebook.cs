1d28d4311ee47a9a1daf65e2ba078d1dc00dbb72
Lucas da Luz <lucasdaluz0399@gmail.com>
2021-05-13 20:08:43 -0300

    initial commit

---
+++ Models/Database/Phonebook.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models
+{
+    [Table("phonebook")]
+    public class Phonebook
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("name")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [Column("nickname")]
+        [MaxLength(20)]
+        public string Nickname { get; set; }
+
+        [ForeignKey("User")]
+        [Column("idUser")]
+        public int IdUser { get; set; }
+    }
+}
---
+++ Models/Database/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models
+{
+    [Table("user")]
+    public class User
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("username")]
+        [MaxLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        [Column("password")]
+        [MaxLength(60)]
+        public string Password { get; set; }
+    }
+}
